963ab875ecf0a12d20884736fddba83cdfd5a519
Cole Reveal <alexi.reveal@gmail.com>
2014-05-10 01:15:48 -0500

    Here is the first form of the running project.

---
+++ roulette.php
@@ -0,0 +1,53 @@
+<?php
+
+// This project is designed to simulate the odds of winning black in European style roulette with the double bet strategy.
+// Winning is defined as doubling your original purse.
+// After every loss, the stratedy is to double your former bet in order to win back all losses from the previous bet.
+// We will play until we lose all our money or double the beginning purse.
+// The odds of winning a single bet on black is 18/37 = 0.48648648648649.
+
+$beginning = 1000;
+$purse = 1000;
+$win = 48649;
+$blackhit = 0;
+$bigblack = 0;
+$redhit = 0;
+$bigred = 0;
+$bet = 1;
+
+do {
+	$spin = mt_rand(0,100000);
+	if($spin <= $win) {
+		$purse += $bet;
+		$blackhit += 1;
+		$redhit = 0;
+		if ($blackhit > $bigblack) {
+			$bigblack = $blackhit;
+		}
+	} else {
+		$purse -= $bet;
+		$redhit += 1;
+		$blackhit = 0;
+		if ($redhit > $bigred) {
+			$bigred = $redhit;
+		}
+		if($bet <75) {
+			$bet *= 2;
+			if($bet > 75) {
+				$bet = 75;
+			}
+		}
+	}
+
+
+}while($purse >= 0 && $purse <= (2 * $beginning));
+
+if($purse >= (2 * $beginning)) {
+	echo "You doubled your original purse!\n";
+} else {
+	echo "You lost all your money! Sorry!\n";
+}
+echo "Your longest black hit streak was {$bigblack}!!\n";
+echo "Your longest non black streak was {$bigred}!!\n";
+
+?>

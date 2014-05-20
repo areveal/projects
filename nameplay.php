d78870653fcffdc9a87282612c5bff4daad4d05f
Cole Reveal <alexi.reveal@gmail.com>
2014-05-20 10:15:26 -0500

    added functionality to nameplay game

--- nameplay.php
+++ nameplay.php
@@ -1,39 +1,81 @@
 <?php
 
+//************************************************************
+//************************************************************
+//                     The Functions
+//************************************************************
+//************************************************************
+
+
+
+// Create the name game function
+function name_game($name) {
+
+	//define vowels
+	$vowels = ['a','e','i','o','u'];
+
+	$song = '';
+
+	if(array_search(strtolower($name[0]), $vowels) !== false) {
+		$song .= "{$name}!" . PHP_EOL . 
+		"{$name}, {$name} bo B" . strtolower($name) . PHP_EOL . 
+		"Bonana fanna fo F" . strtolower($name) . PHP_EOL . 
+		"Fee fy mo M" . strtolower($name) . "," . PHP_EOL . 
+		"{$name}! ";
+	} else {
+		$song .= "{$name}!" . PHP_EOL . 
+		"{$name}, {$name} bo B" . substr($name, 1) . PHP_EOL . 
+		"Bonana fanna fo F" . substr($name, 1) . PHP_EOL . 
+		"Fee fy mo M" . substr($name, 1) . "," . PHP_EOL . 
+		"{$name}! ";
+	}
+
+	return $song;
+}
+
+//grab user's name
+function get_name() {
+	
+	echo "What is your name? ";
+
+	$name = (trim(fgets(STDIN)));
+
+	return $name;
+}
+
+// function to make sure name is a name.
+function check_name($name) {
+	if(is_numeric($name)) {
+		echo "Play Fair!!!\n";
+		exit(1);
+	}
+	if(empty($name)) {
+		echo "You must enter a name!\n";
+		exit(1);
+	}
+}
+
+//************************************************************
+//************************************************************
+//                  Here is the actual game!!
+//************************************************************
+//************************************************************
+
+
 echo "Let's play the name game!!\n";
 
 // Prompt the User for their name.
-echo "What is your name? ";
+$name = get_name();
 
-$name = (trim(fgets(STDIN)));
-
-// Make sure they gave you a word and not a number.
-if(is_numeric($name)) {
-	echo "Play Fair!!!\n";
-	exit(1);
-}
-if(empty($name)) {
-	echo "You must enter a name!\n";
-	exit(1);
-}
+// Make sure they gave you a word with which the name game is possible.
+check_name($name);
 
 
-$vowels = ['a','e','i','o','u'];
+// Here is the name game!!
+echo name_game($name);
 
-// Here is the name game!
-if(is_int(array_search(strtolower($name[0]), $vowels))) {
-	echo "{$name}!" . PHP_EOL . 
-	"{$name}, {$name} bo B" . strtolower($name) . PHP_EOL . 
-	"Bonana fanna fo F" . strtolower($name) . PHP_EOL . 
-	"Fee fy mo M" . strtolower($name) . "," . PHP_EOL . 
-	"{$name}! ";
-} else {
-	echo "{$name}!" . PHP_EOL . 
-	"{$name}, {$name} bo B" . substr($name, 1) . PHP_EOL . 
-	"Bonana fanna fo F" . substr($name, 1) . PHP_EOL . 
-	"Fee fy mo M" . substr($name, 1) . "," . PHP_EOL . 
-	"{$name}! ";
-}
+echo PHP_EOL;
+exit(0);
 
- echo PHP_EOL;
+
 ?>

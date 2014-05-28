8b9bc0b093763dd6ff99135ccd3b854662a2486f
Cole Reveal <alexi.reveal@gmail.com>
2014-05-28 13:20:03 -0500

    cleaned up diceware functions file

--- codeup_challenges/diceware.php
+++ codeup_challenges/diceware.php
@@ -1,9 +1,21 @@
 <?php
 
+function get_input() {
+	
+	fwrite(STDOUT, 'How many words do you want for your passphrase? ');
+	$input = trim(fgets(STDIN));
 
-function diceware($num) {
-	//read the file
-	$filename = 'diceware.txt';
+	while(!is_numeric($input)) {
+		fwrite(STDOUT, 'Input must be a number: ');
+		$input = trim(fgets(STDIN));
+	}
+
+	return $input;
+
+}
+
+function read_file($filename) {
+
 	$filesize = filesize($filename);
 	$read = fopen($filename, 'r');
 	$long_list = trim(fread($read, $filesize));
@@ -11,16 +23,13 @@ function diceware($num) {
 
 	//explode it into pieces
 	$pieces = explode("\n", $long_list);
+	return $pieces;
+}
 
-	//break the pieces into two parts
-	$parts = [];
-	foreach ($pieces as $piece) {
-		$parts[] = explode("\t", $piece);
-	}
+function get_pass($int, $array) {
 	
 	$passphrase = "";
-	//assign password into passphrase $num times
-	for ($i=0; $i < $num; $i++) { 
+	for ($i=0; $i < $int; $i++) { 
 		//this will ultimately be our passphrase
 		//we're going to get our key here.
 		$code = "";
@@ -32,7 +41,7 @@ function diceware($num) {
 		//here is our number
 		$code .= implode($code_array);
 		//add our word to the passphrase
-		foreach ($parts as $key => $part) {
+		foreach ($array as $key => $part) {
 			if($part[0] == $code) {
 				$passphrase .= $part[1];
 			}
@@ -40,22 +49,31 @@ function diceware($num) {
 
 		
 	}
+	return $passphrase;
+
+} 
+
+function diceware($num) {
+	//read the file
+	$pieces = read_file('diceware.txt');
+	//break the pieces into two parts
+	$parts = [];
+	foreach ($pieces as $piece) {
+		$parts[] = explode("\t", $piece);
+	}
 	
+	//assign password into passphrase $num times
+	$passphrase = get_pass($num, $parts);
 	//give back the completed passphrase
 	return $passphrase;
 
 }
 
+//actually do it!!
+$input = get_input();
 
+$passphrase = diceware($input);
 
-fwrite(STDOUT, 'How many words do you want for your passphrase? ');
-$input = trim(fgets(STDIN));
-
-while(!is_numeric($input)) {
-	fwrite(STDOUT, 'Input must be a number: ');
-	$input = trim(fgets(STDIN));
-}
-
-echo diceware($input) . PHP_EOL;
+fwrite(STDOUT, $passphrase . PHP_EOL);
 
 ?>
